62fc23b940231228c388c9f2de2657c4a905126a
dflowers7 <dflowers@mit.edu>
2017-04-06 11:48:12 -0400

    Fixed bug where requested number of workers was ignored
    Also added a printout listing the number of requested workers.

--- InitializeParallel.m
+++ InitializeParallel.m
@@ -6,6 +6,7 @@ delete(gcp('nocreate'))
 
 % Restart pool with desired number of workers
 clu = parcluster;
+fprintf('Requesting %d workers...\n', numworkersperjob)
 clu.NumWorkers = numworkersperjob;
 isstarted = false;
 pausestate = pause('on');
@@ -13,16 +14,24 @@ pausereset = onCleanup(@()pause(pausestate));
 retrycounter = 1;
 while ~isstarted
     try
-        parpool(clu);
+        parpool(clu, numworkersperjob);
         isstarted = true;
     catch ME
         switch ME.identifier
             case 'parallel:cluster:PoolRunValidation'
                 if retrycounter > 10
+                    fprintf(getReport(ME))
+                    fprintf('\n')
                     fprintf('Starting parallel pool failed too many times! Aborting!\n')
                     rethrow(ME)
                 else
+                    fprintf(getReport(ME))
+                    fprintf('\n')
                     fprintf('Starting parallel pool failed. Retrying in 10 seconds...\n')
+                    retrycounter = retrycounter + 1;
+                    delete(gcp('nocreate'))
+                    clu = parcluster;
+                    clu.NumWorkers = numworkersperjob;
                     pause(10)
                 end
             otherwise
